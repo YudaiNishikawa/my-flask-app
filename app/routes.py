3fe3a639b1ece911f6d08e6d86be9df1fcc39a2c
YudaiNishikawa <universitystudent@keio.jp>
2026-01-01 16:17:55 +0900

    第2章完了：テンプレートの継承とリスト表示の実装

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
-@app.route('/') # トップページ（/）にアクセスしたら...
-@app.route('/index') # /index にアクセスしても...
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Hello, World!" # この文字を画面に出して！
+    # ユーザーのダミーデータ（本当はデータベースから取ってきますが、今は手動で）
+    user = {'username': 'ユウダイ',"favorite_food": "えびフィレオ"}
+
+    posts=[
+        {
+            "author":{"username":"ジョン"}
+            ,"body":"今日は天気がいいね！"
+        },
+        {
+            "author":{"username":"スーザン"},
+            "body":"アベンジャーズの映画、最高だった！"
+        },
+        {
+            "author":{"username":"yudai"},
+            "body":"pythonの勉強中！"
+        }
+    ]
+    
+    return render_template('index.html', title='ホーム', user=user,posts=posts)
+
